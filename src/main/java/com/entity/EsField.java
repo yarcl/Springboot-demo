8414ea3c2a6394d1d57774cc7f178e5030bf0460
dingjian <ssi-dingj@dfmc.com.cn>
2020-02-21 11:51:42 +0800

    测试代码

--- src/main/java/com/entity/EsField.java
+++ src/main/java/com/entity/EsField.java
@@ -5,9 +5,13 @@ package com.entity;
  */
 public class EsField {
 
+    //测试3
+
     public static final String ID = "_id";
     public static final String SCHEMA = "_schema";
     public static final String SCORE = "_score";
+
+
 
     public static final String NAME = "name";
 }
